99f368bdce4624a036f4aa7c8967b49bc2504649
Javier Lozano <j.loz.gwyn@gmail.com>
2022-11-09 01:18:51 -0600

    + starfield.c Activado Alpha Blending y se agrego un efecto de 'Barrido'

--- Starfield/C/starfield.c
+++ Starfield/C/starfield.c
@@ -15,13 +15,12 @@
 #define HEIGHT 480
 
 #define STARS 500
-#define SPAWN_RANGE WIDTH * 0.5
+#define STAR_SPEED_MIN 1
+#define STAR_SPEED_MAX 7
+#define TRAIL 5
 
-#define SPD_ACCEL 0.5
-#define SPD_MAX 8
-#define SPD_RANGE_MAX (SPD_MAX / SPD_ACCEL) - 1
-
-#define BRIGHT_RATE 5
+#define ALPHA_MIN 0
+#define ALPHA_RATE 5
 
 #define PI 3.1416
 
@@ -34,8 +33,10 @@ typedef struct {
 
 typedef struct {
 	Vector2 position;
-	Vector2 speed;
-	unsigned int color;
+	Vector2 trail[TRAIL];
+	Vector2 angle;
+	float speed;
+	SDL_Color color;
 } Star;
 
 typedef struct {
@@ -47,7 +48,7 @@ typedef struct {
 // Prototipos
 
 void ConstructStar(Star*);
-void UpdateStars(Star*, SDL_Renderer*, Vector2* origin);
+void UpdateStar(Star*, SDL_Renderer*, Vector2* origin);
 
 float RandFloat(float min, float max);
 
@@ -63,6 +64,9 @@ int main(int argc, char* args[])
 	demo->window = SDL_CreateWindow("SDL Starfield C", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_BORDERLESS);
 	demo->renderer = SDL_CreateRenderer(demo->window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
+	// Activa el Alpha Blending
+	SDL_SetRenderDrawBlendMode(demo->renderer, SDL_BLENDMODE_BLEND);
+
 	// Semilla aleatorea en base a la "Tiempo" actual 
 	srand(time(NULL));
 
@@ -70,6 +74,7 @@ int main(int argc, char* args[])
 	Star* starfield = malloc(sizeof(Star) * STARS);
 	for (int i = 0; i < STARS; i++)
 	{
+		//printf("Min Vel: %d Max Vel: %d Actual: %f\n", STAR_SPEED_MIN, STAR_SPEED_MAX, velocidad);
 		ConstructStar(&starfield[i]);
 	}
 
@@ -93,7 +98,10 @@ int main(int argc, char* args[])
 		//SDL_RenderDrawLine(demo->renderer, 0, origen.y, WIDTH, origen.y);
 
 		// Actualiza Estrellas 
-		UpdateStars(starfield, demo->renderer, &origen);
+		for (int i = 0; i < STARS; i++)
+		{
+			UpdateStar(&starfield[i], demo->renderer, &origen);
+		}
 
 		// Dibuja en Pantalla 
 		SDL_RenderPresent(demo->renderer);
@@ -115,63 +123,74 @@ int main(int argc, char* args[])
 
 // Definicion 
 
-void UpdateStars(Star* starfield, SDL_Renderer* renderer, Vector2* origen)
-{
-	for (int i = 0; i < STARS; i++)
-	{
-		Vector2 p = { starfield[i].position.x + origen->x, starfield[i].position.y + origen->y };
-		// Revisa si la estrella estan dentro de la ventana 
-		if (p.x < 0 || p.x > WIDTH || p.y < 0 || p.y > HEIGHT)
-		{
-			starfield[i].position.x = 0;
-			starfield[i].position.y = 0;
-			starfield[i].color = 0;
-		}
-
-		// Mueve las Estrellas 
-		starfield[i].position.x += starfield[i].speed.x;
-		starfield[i].position.y += starfield[i].speed.y;
-
-		// Color 
-		starfield[i].color += BRIGHT_RATE;
-		if (starfield[i].color > 0xFF)
-		{
-			starfield[i].color = 0xFF;
-		}
-
-		// Manda a Dibujar las Estrellas 
-		SDL_SetRenderDrawColor(renderer, starfield[i].color, starfield[i].color, starfield[i].color, 0xFF);
-		SDL_RenderDrawPoint(renderer, p.x, p.y);
-	}
-}
-
 void ConstructStar(Star* star)
 {
 	// Obten un Angulo aleatoreo entre 0 y 2PI randianes 
 	float angulo = RandFloat(0, PI * 2);
 
 	// Calcula Seno y Coseno 
-	float cos = SDL_cosf(angulo);
-	float sen = SDL_sinf(angulo);
+ 	star->angle.x = SDL_cosf(angulo);
+	star->angle.y = SDL_sinf(angulo);
 
-	// Obten numero aleatoreo 
-	float radio = rand() % SPAWN_RANGE;
-
-	// Inicializamos la estrella  
-	star->position.x = radio * cos;
-	star->position.y = radio * sen;
-
-	// Esta formula determina la velocidad en la que la estrella viaja:
-	// velocidad = (random() * -inclinacion) + elevacion
-	float random = RandFloat(0, SPD_RANGE_MAX);
-	float velocidad = (random * -SPD_ACCEL) + SPD_MAX;
+	// Obten la Velocidad aleatorea entre STAR_SPEED_MIN y STAR_SPEED_MAX
+	star->speed = RandFloat((float)STAR_SPEED_MIN, (float)STAR_SPEED_MAX);
 	
-	star->speed.x = cos * velocidad;
-	star->speed.y = sen * velocidad;
+	// Inicializamos la posicion de la estrella 
+	star->position.x = 0;
+	star->position.y = 0;
+
+	// Inicializamos la cola (trail)
+	for (int i = 0; i < TRAIL; i++)
+	{
+		star->trail[i] = star->position;
+	}
+	
+	// Color 
+	star->color.a = 0xFF;
+	//printf("Color: 0x%d\n", star->color);
+}
+
+void UpdateStar(Star* star, SDL_Renderer* renderer, Vector2* origen)
+{
+	Vector2 p = { star->position.x + origen->x, star->position.y + origen->y };
+	// Revisa si la estrella estan dentro de la ventana 
+	if (p.x < 0 || p.x > WIDTH || p.y < 0 || p.y > HEIGHT)
+	{
+		star->position.x = 0;
+		star->position.y = 0;
+		star->color.a = ALPHA_MIN;
+	}
+
+	//
+	for (int i = TRAIL - 1; i > 0 ; i--)
+	{
+		star->trail[i] = star->trail[i - 1];
+	}
+	star->trail[0] = star->position;
+
+	// Mueve las Estrellas 
+	star->position.x += star->speed * star->angle.x;
+	star->position.y += star->speed * star->angle.y;
 
 	// Color 
-	star->color = 0x55;
-	//printf("Color: 0x%d\n", star->color);
+	int alpha = star->color.a + ALPHA_RATE;
+	if (alpha > 0xFF)
+	{
+		star->color.a = 0xFF;
+	}
+	else
+	{
+		star->color.a = alpha;
+	}
+
+	// Manda a Dibujar las Estrellas 
+	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, star->color.a);
+	SDL_RenderDrawPoint(renderer, p.x, p.y);
+	for (int i  = 0; i < TRAIL; i++)
+	{
+		SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, star->color.a / (i+1));
+		SDL_RenderDrawPoint(renderer, star->trail[i].x + origen->x, star->trail[i].y + origen->y);
+	}
 }
 
 float RandFloat(float min, float max)
